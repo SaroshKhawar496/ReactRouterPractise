5693495d9dcb8e6fad18c31621d007d8aca24af3
SaroshKhawar496 <sarosh.496@gmail.com>
2019-09-25 01:01:54 -0400

    React Router App

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea esse
+        accusantium laudantium ex nulla earum iste quae illo porro deserunt
+        doloremque excepturi, architecto sapiente, aperiam hic exercitationem
+        possimus. Vero, qui!
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea esse
+        accusantium laudantium ex nulla earum iste quae illo porro deserunt
+        doloremque excepturi, architecto sapiente, aperiam hic exercitationem
+        possimus. Vero, qui!
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
