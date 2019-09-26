a3761c11a31c5bd2655f07eba4719a664c863d59
SaroshKhawar496 <sarosh.496@gmail.com>
2019-09-26 18:36:01 -0400

    Installed axios. Calling json api in Home in componentDidUpdate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea esse
-        accusantium laudantium ex nulla earum iste quae illo porro deserunt
-        doloremque excepturi, architecto sapiente, aperiam hic exercitationem
-        possimus. Vero, qui!
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  //lifecycle method in which you can make api calls to server/db
+  componentDidMount() {
+    //request below is async, returns a promise
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0.1) //getting first 10 posts from api
+      });
+    });
+  }
+
+  render() {
+    const posts = this.state.posts;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No Posts Yet!</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 
 export default Home;
