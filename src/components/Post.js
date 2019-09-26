255e000eef50e6b0725b153904db0fffeb7c52c3
SaroshKhawar496 <sarosh.496@gmail.com>
2019-09-26 19:02:09 -0400

    Showing individual post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Post extends Component {
   state = {
-    id: null
+    post: null
   };
   componentDidMount() {
     //props automatically being passed down by react router.
     let id = this.props.match.params.post_id;
-    this.setState({
-      id: id
+    axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
+      this.setState({
+        post: res.data
+      });
     });
   }
   render() {
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading Post ....</div>
+    );
     return (
       <div className="container">
-        <h4>{this.state.id}</h4>
+        <h4>{post}</h4>
       </div>
     );
   }
