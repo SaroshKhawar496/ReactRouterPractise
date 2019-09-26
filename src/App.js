96e2c0228758ca5071326d1c1d30937ac0bf5388
SaroshKhawar496 <sarosh.496@gmail.com>
2019-09-26 19:06:41 -0400

    Added Swtich from react-router-dom to only show one component from similar looking routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
-import Post from './components/Post';
+import Post from "./components/Post";
 
 class App extends Component {
   render() {
@@ -17,12 +17,15 @@ class App extends Component {
             automatically. like the history. It doesn't pass to non-Route componnets
              unless you wrap them in HOC
            */}
-
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          {/* Route for individual post */}
-          <Route path="/:post_id" component={Post}/>
+          {/* switch tag -> only one route takes precedence, don't match 2 at a time. 
+          like /contact or /about wont' match /:postid */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            {/* Route for individual post */}
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
