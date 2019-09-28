1fd9cc904495be78146f507480a4bb82210ff75a
SaroshKhawar496 <sarosh.496@gmail.com>
2019-09-28 02:14:06 -0400

    Added btn to delete a post, using mapDispatchToProps to invoke delete action

--- src/components/Post.js
+++ src/components/Post.js
@@ -15,12 +15,25 @@ class Post extends Component {
   //     });
   //   });
   // }
+
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    //redirecting user to home
+    this.props.history.push("/");
+  };
+
   render() {
+    console.log(this.props);
     // const post = this.state.post ? (
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading Post ....</div>
@@ -32,6 +45,8 @@ class Post extends Component {
     );
   }
 }
+//mapStateToProps is used for selecting the part of the data from the store
+//that the connected component needs.
 //second param is the props of this own component before we attach the redux store
 const mapStateToProps = (state, ownProps) => {
   //grabbing the post id via ownProps (ie from react-router)
@@ -41,4 +56,28 @@ const mapStateToProps = (state, ownProps) => {
     post: state.posts.find(post => post.id === id)
   };
 };
-export default connect(mapStateToProps)(Post);
+
+//mapDispatchToProps is used for dispatching actions to the store.
+//dispatch is a function of the Redux store. You call store.dispatch
+//to dispatch an action. This is the only way to trigger a state change.
+const mapDispatchToProps = dispatch => {
+  return {
+    //deletePost is a fxn we will call via props in thsi component,
+    // to delete post from store
+    //id of post we want to delete
+    deletePost: id => {
+      dispatch(
+        // its the action that we are dispatching
+        {
+          type: "DELETE_POST",
+          id: id
+        }
+      );
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Post);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -8,6 +8,15 @@ const initState = {
 
 //first time it runs, we pass initState to store via this rootReducer
 const rootReducer = (state = initState, action) => {
+  if (action.type === "DELETE_POST") {
+    let newPosts = state.posts.filter(post => {
+      return post.id !== action.id;
+    });
+    return {
+      ...state, //spread what is already in the state
+      posts: newPosts
+    };
+  }
   return state;
 };
 
